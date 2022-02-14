6077f4b17b7f6497803f0e0f2ba1e25a9ac344c2
Vitória Pizzutti <vitoria@animati.com.br>
2022-02-13 23:18:00 -0300

    Filling main method.

--- src/com/vitoria/Main.java
+++ src/com/vitoria/Main.java
@@ -1,8 +1,24 @@
 package com.vitoria;
 
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Normaliser normaliser = new Normaliser();
+
+        System.out.println("Write a job title: ");
+        String value = scanner.nextLine();
+
+        String normalisedString = normaliser.normalise(value);
+        if (StringUtils.isEmpty(normalisedString)) {
+            System.out.println("No results.");
+        } else {
+            System.out.println(((value.equals(normalisedString)) ?
+                    "You searched for " : "You must be searching for ") + normalisedString);
+        }
     }
 }
